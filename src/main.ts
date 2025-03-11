16d7c13b3979a698cfb30a0eb1c31b87821f64cd
Michiel de Jong <michiel@pondersource.com>
2025-03-11 11:23:06 +0100

    automerge hello world

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,29 @@ import { WebSocketServer } from "ws";
 import { NodeWSServerAdapter } from "@automerge/automerge-repo-network-websocket";
 import { NodeFSStorageAdapter } from "@automerge/automerge-repo-storage-nodefs";
 
-const wss = new WebSocketServer({ noServer: true });
+const wss = new WebSocketServer({ port: 8080 });
 
-new Repo({
+const repo1 = new Repo({
   network: [new NodeWSServerAdapter(wss as any)],
   storage: new NodeFSStorageAdapter('./data'),
+});
+
+// const repo2 = new Repo({
+//   network: [new NodeWSServerAdapter(wss as any)],
+//   storage: new NodeFSStorageAdapter('./data'),
+// });
+
+console.log('creating doc');
+const doc = repo1.create();
+console.log('setting doc text');
+doc.change((d: { text: string }) => {
+  d.text = 'hello'
+});
+console.log('waiting for change event');
+doc.on('change', ({ doc }) => {
+  console.log("new text is", (doc as any).text);
+});
+console.log('changing doc');
+doc.change((d: { text: string }) => {
+  d.text += ' world'
 });
