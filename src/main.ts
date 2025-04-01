e90e951de12920564ae884c0f1e5d29ad9cc6f00
Michiel de Jong <michiel@pondersource.com>
2025-04-01 16:53:14 +0200

    headers as unknown as any

--- src/main.ts
+++ src/main.ts
@@ -54,7 +54,8 @@ async function toBackend(data: {
   const upstreamRes = await fetch(`${data.backendUrl}${data.path}`, params);
   const resBody = await upstreamRes.text();
   const resHeaders: { [key: string]: string } = {};
-  for (const header of upstreamRes.headers as any) {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  for (const header of upstreamRes.headers as unknown as any) {
     resHeaders[header[0]] = header[1];
   }
 
@@ -135,51 +136,3 @@ async function run(): Promise<void> {
 
 // ...
 run();
-
-// /transaction/relay {
-// "uuid":"27d4e339-c69d-4aa3-9c38-1eeb4f17bb3a",
-// "state":"V",
-// "workflow":"_C0",
-// "entries":[
-// {"payee":"trunk\/branch\/twig\/alice",
-// "payer":"branch\/bob",
-// "quant":72,
-// "description":"test without confirmation",
-// "metadata":{"foo":"bar"}},
-// {"payee":"trunk\/branch\/twig\/admin",
-// "payer":"branch\/bob",
-// "quant":24,
-// "description":"Payer fee of 1 to twig\/admin",
-// "metadata":{}}]}
-
-// /transaction/relay {
-// "uuid":"d2cf94ca-9d2e-4951-9b6e-73824c40a83f",
-// "state":"V",
-// "workflow":"_C0CE-PE0CX-",
-// "entries":[
-// {"payee":"trunk\/branch\/twig\/alice",
-// "payer":"branch\/bob",
-// "quant":192,
-// "description":"test near instant bill",
-// "metadata":{"foo":"bar"}},
-// {"payee":"trunk\/branch\/twig\/admin",
-// "payer":"branch\/bob",
-// "quant":24,
-// "description":"Payer fee of 1 to twig\/admin",
-// "metadata":{}}]}
-
-// /transaction/relay {
-// "uuid":"a068b3b3-b554-4465-a6d1-e8deef9a953e",
-// "state":"V",
-// "workflow":"|P-PC+CX+",
-// "entries":[
-// {"payee":"branch2\/bob",
-// "payer":"trunk\/branch\/twig\/alice",
-// "quant":72,
-// "description":"test long distance for 3 from leaf",
-// "metadata":{"foo":"bar"}},
-// {"payee":"trunk\/branch\/twig\/admin",
-// "payer":"branch2\/bob",
-// "quant":24,
-// "description":"Payee fee of 1 to twig\/admin",
-// "metadata":{}}]}
