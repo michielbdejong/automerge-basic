a587cdd37ccd7045371dc2bb57f1aba866198810
Michiel de Jong <michiel@pondersource.com>
2025-05-06 16:59:32 +0200

    receive from both sides. todo: copy through to the other side

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import { createServer, IncomingMessage, ServerResponse } from 'http';
-// import { Tub } from './tub.js';
-// import { SlackClient, IMessage } from './SlackClient.js';
+import { Tub } from './tub.js';
+import { SlackClient, IMessage } from './SlackClient.js';
 import { SolidClient } from './SolidClient.js';
 
 
@@ -13,11 +13,14 @@ createServer((req: IncomingMessage, res: ServerResponse) => {
 }).listen(8080);
 
 async function run(): Promise<void> {
-  // const tub1 = new Tub('1');
-  // const tub2 = new Tub('2');
-  // const docUrl = await tub1.createDoc();
-  // await startSlackClient(tub1);
-  // await tub2.setDoc(docUrl);
+  const tub1 = new Tub('1');
+  const tub2 = new Tub('2');
+  const docUrl = await tub1.createDoc();
+  await startSlackClient(tub1);
+  await tub2.setDoc(docUrl);
+  await startSolidClient(tub2);
+}
+async function startSolidClient(tub: Tub): Promise<void> {
   const solid = new SolidClient();
   await solid.connect();
   const topic = process.env.SOLID_TOPIC;
@@ -34,40 +37,47 @@ async function run(): Promise<void> {
       },
     });
     const chatChannel = await docRes.json();
-    chatChannel.forEach(entry => {
+    const tubsChannelId = await tub.getId(makeLocalId(['solid', 'channel', topic]));
+    await Promise.all(chatChannel.map(async (entry) => {
       if (entry['@id'].startsWith(topic)) {
         if (Array.isArray(entry['http://rdfs.org/sioc/ns#content']) && entry['http://rdfs.org/sioc/ns#content'].length === 1) {
           console.log(entry['@id'], entry['http://rdfs.org/sioc/ns#content'][0]['@value']);
+          const msgId = await tub.getId(makeLocalId(['solid', 'message', entry['@id']]));
+          tub.setData(msgId, {
+            id: msgId,
+            text: entry['http://rdfs.org/sioc/ns#content'][0]['@value'],
+            channel: tubsChannelId,
+          });
         }
       }
-    })
+    }));
   }
 }
 
-// function makeLocalId(parts: string[]): string {
-//   return parts.join(':');
-// }
-// async function startSlackClient(tub: Tub): Promise<void> {
-//   const slackClient = new SlackClient();
-//   await slackClient.create('');
-//   await slackClient.start(8080);
-//   slackClient.on('message', async (message: IMessage) => {
-//     console.info('----------onMessage-----------');
-//     const tubsChannelId = await tub.getId(
-//       makeLocalId(['slack', 'channel', message.channel]),
-//     );
-//     const tubsMsgId = await tub.getId(
-//       makeLocalId(['slack', 'message', message.client_msg_id]),
-//     );
-//     const messageToStore = {
-//       id: tubsMsgId,
-//       text: message.text,
-//       channel: tubsChannelId,
-//     };
-//     tub.setData(tubsMsgId, messageToStore);
-//     console.log(JSON.stringify(message, null, 2));
-//   });
-// }
+function makeLocalId(parts: string[]): string {
+  return parts.join(':');
+}
+async function startSlackClient(tub: Tub): Promise<void> {
+  const slackClient = new SlackClient();
+  await slackClient.create('');
+  await slackClient.start(8080);
+  slackClient.on('message', async (message: IMessage) => {
+    console.info('----------onMessage-----------');
+    const tubsChannelId = await tub.getId(
+      makeLocalId(['slack', 'channel', message.channel]),
+    );
+    const tubsMsgId = await tub.getId(
+      makeLocalId(['slack', 'message', message.client_msg_id]),
+    );
+    const messageToStore = {
+      id: tubsMsgId,
+      text: message.text,
+      channel: tubsChannelId,
+    };
+    tub.setData(tubsMsgId, messageToStore);
+    console.log(JSON.stringify(message, null, 2));
+  });
+}
 
 // ...
 run();
