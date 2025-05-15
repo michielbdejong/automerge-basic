b4b25587ae25c25505caa73b752d3e373fcc6a3c
Michiel de Jong <michiel@pondersource.com>
2025-05-15 14:45:15 +0200

    prettier

--- src/solid/jsonld.ts
+++ src/solid/jsonld.ts
@@ -1,8 +1,9 @@
-export function getJsonLdField(entry: object, pred: string, subPred: string): string | undefined {
-  if (
-    (Array.isArray(entry[pred])) &&
-    (entry[pred].length === 1)
-  ) {
+export function getJsonLdField(
+  entry: object,
+  pred: string,
+  subPred: string,
+): string | undefined {
+  if (Array.isArray(entry[pred]) && entry[pred].length === 1) {
     return entry[pred][0][subPred];
   }
   return undefined;
@@ -19,30 +20,43 @@ export function getJsonLdType(entry: object): string | undefined {
   }
   return undefined;
 }
-  
-export function getJsonLdFieldMulti(entry: object, pred: string, subPred: string): string[] | undefined {
-  if (
-    (Array.isArray(entry[pred])) &&
-    (entry[pred].length === 1)
-  ) {
-    return entry[pred].map(obj => obj[subPred]);
+
+export function getJsonLdFieldMulti(
+  entry: object,
+  pred: string,
+  subPred: string,
+): string[] | undefined {
+  if (Array.isArray(entry[pred]) && entry[pred].length === 1) {
+    return entry[pred].map((obj) => obj[subPred]);
   }
   return undefined;
 }
 
-export function getJsonLdLinkField(entry: object, pred: string): string | undefined {
+export function getJsonLdLinkField(
+  entry: object,
+  pred: string,
+): string | undefined {
   return getJsonLdField(entry, pred, '@id');
 }
 
-export function getJsonLdStringField(entry: object, pred: string): string | undefined {
-    return getJsonLdField(entry, pred, '@value');
-  }
-  
-export function getJsonLdStringFieldMulti(entry: object, pred: string): string[] | undefined {
+export function getJsonLdStringField(
+  entry: object,
+  pred: string,
+): string | undefined {
+  return getJsonLdField(entry, pred, '@value');
+}
+
+export function getJsonLdStringFieldMulti(
+  entry: object,
+  pred: string,
+): string[] | undefined {
   return getJsonLdFieldMulti(entry, pred, '@id');
 }
 
-export function getJsonLdDateField(entry: object, pred: string): Date | undefined {
+export function getJsonLdDateField(
+  entry: object,
+  pred: string,
+): Date | undefined {
   const typeStr = getJsonLdField(entry, pred, '@type');
   if (typeStr === 'http://www.w3.org/2001/XMLSchema#dateTime') {
     const valueStr = getJsonLdStringField(entry, pred);
--- src/solid/testTracker.ts
+++ src/solid/testTracker.ts
@@ -4,9 +4,12 @@ import { fetchTracker, interpret } from './tasks.js';
 
 async function test(): Promise<void> {
   const fetcher = await getFetcher();
-  const { index, state} = await fetchTracker(process.env.TRACKER_IN_SOLID, fetcher);
-//   console.log(JSON.stringify(index, null, 2));
-//   console.log(JSON.stringify(state, null, 2));
+  const { index, state } = await fetchTracker(
+    process.env.TRACKER_IN_SOLID,
+    fetcher,
+  );
+  //   console.log(JSON.stringify(index, null, 2));
+  //   console.log(JSON.stringify(state, null, 2));
   console.log(JSON.stringify(interpret({ index, state }), null, 2));
 }
 
