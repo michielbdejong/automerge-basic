99e82ae310f6f5206aa096b2c073b02ac9bf2f83
Michiel de Jong <michiel@pondersource.com>
2025-05-15 11:18:23 +0200

    start defining the type of the TrackerIndex

--- src/solid/tasks.ts
+++ src/solid/tasks.ts
@@ -1,3 +1,14 @@
+type TrackerIndex = {
+  '@id': string,
+  'http://purl.org/dc/elements/1.1/author': object,
+  '@type': [ 'http://www.w3.org/2005/01/wf/flow#Tracker' ],
+  'http://purl.org/dc/elements/1.1/created': object,
+  'http://www.w3.org/2005/01/wf/flow#issueClass': object,
+  'http://www.w3.org/2005/01/wf/flow#initialState': object,
+  'http://www.w3.org/2005/01/wf/flow#stateStore': object,
+  'http://www.w3.org/2005/01/wf/flow#assigneeClass': object
+};
+
 export async function fetchTracker(
   uri: string,
   authenticatedFetcher: typeof globalThis.fetch,
@@ -7,6 +18,6 @@ export async function fetchTracker(
       Accept: 'application/ld+json',
     },
   });
-  const data = await ret.json();
-  console.log(data);
+  const data: TrackerIndex = await ret.json();
+  console.log(JSON.stringify(data, null, 2));
 }
